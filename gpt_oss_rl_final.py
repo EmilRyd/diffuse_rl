016213f294b5c1008e57c4ed8856d8efd56f5228
Vladimir Ivanov <96141489+astOwOlfo@users.noreply.github.com>
2025-10-20 15:40:04 +0100

    dev

--- gpt_oss_rl_final.py
+++ gpt_oss_rl_final.py
@@ -240,7 +240,7 @@ async def generate_single_completion(
             prompt=prompt_with_chat_template,
             completion=completion["text"],
             completion_tokens=[x["id"] for x in completion["logprobs"]["content"]],
-            completion_logprobs=[x["logprob"] for x in completions["logprobs"]["content"]],
+            completion_logprobs=[x["logprob"] for x in completion["logprobs"]["content"]],
             n_prompt_tokens=response["usage"]["prompt_tokens"],
         )
 
